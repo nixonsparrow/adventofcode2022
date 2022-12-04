bb12b9c17da7443abb6af59220b3f8843b7b40d3
Nikodem Wróbel <nwrobel@bsbox.pl>
2022-12-04 11:35:29 +0100

    Day 4 part 2

--- day4/part2.py
+++ day4/part2.py
@@ -1,4 +1,16 @@
-from utils import read_input
+from part1 import prepare_input
+
+
+def check_overlap(range_a, range_b):
+    for i in range_a:
+        if i in range_b:
+            return True
+    return False
+
 
 if __name__ == '__main__':
-    pass
+    total = 0
+    for x, y in prepare_input("input.txt"):
+        if check_overlap(x, y):
+            total += 1
+    print(total)
